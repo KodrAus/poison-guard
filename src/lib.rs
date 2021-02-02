e06fbd03f8e20227fc6dc02674a85ca22c4eee4c
KodrAus <kodraus@hey.com>
2021-02-02 11:11:53 +1000

    explore init guard

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,111 @@
+pub mod guard {
+    use std::{mem, ops};
+
+    pub struct InitArgs<T, FInit, FUnwind> {
+        pub to_init: T,
+        pub on_init: FInit,
+        pub on_err_unwind: FUnwind,
+    }
+
+    struct InitGuard<T, FUnwind>(Option<mem::ManuallyDrop<T>>, Option<FUnwind>)
+    where
+        FUnwind: FnOnce(&mut T);
+
+    impl<T, FUnwind> ops::Drop for InitGuard<T, FUnwind>
+    where
+        FUnwind: FnOnce(&mut T),
+    {
+        fn drop(&mut self) {
+            if let (Some(mut unwound), Some(on_err_unwind)) = (self.0.take(), self.1.take()) {
+                on_err_unwind(&mut unwound);
+
+                // Since `unwound` is `ManuallyDrop` this won't
+                // actually run any destructors
+                drop(unwound);
+            }
+        }
+    }
+
+    impl<T, FInit, FUnwind> InitArgs<T, FInit, FUnwind>
+    where
+        FUnwind: FnOnce(&mut T),
+    {
+        pub fn init(self) -> T
+        where
+            FInit: FnOnce(&mut T),
+        {
+            let mut guard = InitGuard(Some(mem::ManuallyDrop::new(self.to_init)), Some(self.on_err_unwind));
+            (self.on_init)(guard.0.as_mut().unwrap());
+
+            // If we make it this far then we didn't panic
+            // Dropping the guard after taking the value won't run the unwind destructor
+            let value = guard.0.take().unwrap();
+            drop(guard);
+
+            mem::ManuallyDrop::into_inner(value)
+        }
+
+        pub fn try_init<E>(self) -> Result<T, E>
+        where
+            FInit: FnOnce(&mut T) -> Result<(), E>,
+        {
+            let mut guard = InitGuard(Some(mem::ManuallyDrop::new(self.to_init)), Some(self.on_err_unwind));
+
+            match (self.on_init)(guard.0.as_mut().unwrap()) {
+                // If we make it this far then we didn't panic
+                // The initialization succeeded, so return the value
+                Ok(()) => {
+                    let value = guard.0.take().unwrap();
+
+                    // Dropping the guard here won't run a destructor on `uninit`
+                    drop(guard);
+
+                    Ok(mem::ManuallyDrop::into_inner(value))
+                }
+                // If we make it this far then we didn't panic
+                // The initialization failed, so run the unwind destructor
+                Err(err) => {
+                    // Dropping the guard here will run a destructor on `uninit`
+                    drop(guard);
+
+                    Err(err)
+                }
+            }
+        }
+    }
+}
+
 pub mod poison {
     use std::{fmt, ops, panic};
 
     /**
     A container that holds a potentially poisoned value.
     */
+    // NOTE: This needs to live in `std`, not `core` because
+    // it interacts with unwinding.
     pub struct Poison<T> {
-        // TODO: This could be a `u8` to save space
+        // TODO: This could be a `u8` to save space when combining with other flags
         poisoned: bool,
+        recover_on_drop: bool,
         // TODO: Are there any opportunities to protect invalid state better?
+        // TODO: Consider a `Result<T, PoisonPayload>`?
+        // TODO: Where PoisonPayload may be PanicPayload or Error + Send + Sync + 'static?
         value: T,
     }
 
     impl Poison<()> {
+        // TODO: Make this work on methods returning `T`
+        // TODO: This means making `Poison` capture a `Option<T>`
         pub fn catch_unwind(f: impl FnOnce()) -> Self {
             match panic::catch_unwind(panic::AssertUnwindSafe(f)) {
                 Ok(()) => Poison {
                     poisoned: false,
+                    recover_on_drop: true,
                     value: (),
                 },
                 Err(_) => Poison {
                     poisoned: true,
+                    recover_on_drop: true,
                     value: (),
                 },
             }
@@ -33,6 +119,7 @@ pub mod poison {
         pub fn new(value: T) -> Self {
             Poison {
                 poisoned: false,
+                recover_on_drop: true,
                 value,
             }
         }
@@ -66,10 +153,11 @@ pub mod poison {
                 })
             } else {
                 target.poisoned = true;
+                let recover_on_drop = target.recover_on_drop;
 
                 Ok(PoisonGuard {
                     target,
-                    unpoison_on_drop: true,
+                    recover_on_drop,
                     _marker: Default::default(),
                 })
             }
@@ -84,7 +172,7 @@ pub mod poison {
         Target: ops::DerefMut<Target = Poison<T>>,
     {
         target: Target,
-        unpoison_on_drop: bool,
+        recover_on_drop: bool,
         _marker: std::marker::PhantomData<&'a mut T>,
     }
 
@@ -93,8 +181,8 @@ pub mod poison {
         Target: ops::DerefMut<Target = Poison<T>>,
     {
         // TODO: Call this something more semantic? `poison_on_early_return`? `enter`/`exit`?
-        pub fn unpoison_on_drop(&mut self, unpoison_on_drop: bool) {
-            self.unpoison_on_drop = unpoison_on_drop;
+        pub fn recover_on_drop(&mut self, recover_on_drop: bool) {
+            self.recover_on_drop = recover_on_drop;
         }
     }
 
@@ -104,7 +192,7 @@ pub mod poison {
     {
         fn drop(&mut self) {
             // TODO: Also check if we're unwinding
-            if self.unpoison_on_drop {
+            if self.recover_on_drop {
                 self.target.poisoned = false;
             }
         }
@@ -145,12 +233,13 @@ pub mod poison {
     where
         Target: ops::DerefMut<Target = Poison<T>>,
     {
-        pub fn unpoison(mut self, f: impl FnOnce(&mut T)) -> PoisonGuard<'a, T, Target> {
+        // TODO: Will this always just be the same function if recovery is possible?
+        pub fn recover(mut self, f: impl FnOnce(&mut T)) -> PoisonGuard<'a, T, Target> {
             f(&mut self.target.value);
 
             PoisonGuard {
                 target: self.target,
-                unpoison_on_drop: true,
+                recover_on_drop: true,
                 _marker: Default::default(),
             }
         }
@@ -168,8 +257,8 @@ pub mod poison {
 
 #[cfg(test)]
 mod tests {
-    use crate::poison::*;
-    use std::ops;
+    use crate::{guard::*, poison::*};
+    use std::{mem, ops, ptr};
 
     #[test]
     fn it_works() {
@@ -197,7 +286,7 @@ mod tests {
         // Unpoison the guard and decrement the value back down
         let guard = v
             .poison()
-            .unwrap_or_else(|guard| guard.unpoison(|v| *v = 42));
+            .unwrap_or_else(|guard| guard.recover(|v| *v = 42));
 
         assert_eq!(42, *guard);
         drop(guard);
@@ -213,79 +302,244 @@ mod tests {
         assert!(v.is_poisoned());
     }
 
-    // An example wrapper for `parking_lot::Mutex`
-    // This implements an inherent method on `MutexGuard<Poison<T>>` that shadows `Poison<T>`
-    // We do this deliberately, knowing that the shadowed method is what we want
-    // This is roughly what a new non-poisoning `Mutex` API would look like
-    pub struct Mutex<T>(parking_lot::Mutex<T>);
+    mod mutex {
+        use super::*;
 
-    pub struct MutexGuard<'a, T>(parking_lot::MutexGuard<'a, T>);
+        // An example wrapper for `parking_lot::Mutex`
+        // This implements an inherent method on `MutexGuard<Poison<T>>` that shadows `Poison<T>`
+        // We do this deliberately, knowing that the shadowed method is what we want
+        // This is roughly what a new non-poisoning `Mutex` API would look like
+        pub struct Mutex<T>(parking_lot::Mutex<T>);
 
-    impl<T> Mutex<T> {
-        pub fn new(value: T) -> Self {
-            Mutex(parking_lot::Mutex::new(value))
+        pub struct MutexGuard<'a, T>(parking_lot::MutexGuard<'a, T>);
+
+        impl<T> Mutex<T> {
+            pub fn new(value: T) -> Self {
+                Mutex(parking_lot::Mutex::new(value))
+            }
+
+            pub fn lock(&self) -> MutexGuard<T> {
+                MutexGuard(self.0.lock())
+            }
         }
 
-        pub fn lock(&self) -> MutexGuard<T> {
-            MutexGuard(self.0.lock())
+        impl<'a, T> MutexGuard<'a, Poison<T>> {
+            /**
+            Poison a locked value.
+            */
+            pub fn poison(
+                self,
+            ) -> Result<
+                PoisonGuard<'a, T, MutexGuard<'a, Poison<T>>>,
+                PoisonRecover<'a, T, MutexGuard<'a, Poison<T>>>,
+            > {
+                Poison::poison_target(self)
+            }
+        }
+
+        impl<'a, T> ops::Deref for MutexGuard<'a, T> {
+            type Target = T;
+
+            fn deref(&self) -> &T {
+                &*self.0
+            }
+        }
+
+        impl<'a, T> ops::DerefMut for MutexGuard<'a, T> {
+            fn deref_mut(&mut self) -> &mut T {
+                &mut *self.0
+            }
+        }
+
+        #[test]
+        fn poisoning_mutex() {
+            let mutex = Mutex::new(Poison::new(42));
+
+            let mut guard = mutex.lock().poison().unwrap();
+
+            *guard = 43;
+            drop(guard);
+
+            let guard = mutex.lock();
+
+            assert!(!guard.is_poisoned());
+
+            let guard = guard.poison().unwrap();
+            assert_eq!(43, *guard);
+            drop(guard);
+
+            // Poison the guard without deadlocking the mutex
+            let mut v = mutex.lock().poison().unwrap();
+            v.recover_on_drop(false);
+            drop(v);
+
+            let guard = mutex
+                .lock()
+                .poison()
+                .unwrap_or_else(|guard| guard.recover(|v| *v = 42));
+
+            assert_eq!(42, *guard);
         }
     }
 
-    impl<'a, T> MutexGuard<'a, Poison<T>> {
-        /**
-        Poison a locked value.
-        */
-        pub fn poison(
-            self,
-        ) -> Result<
-            PoisonGuard<'a, T, MutexGuard<'a, Poison<T>>>,
-            PoisonRecover<'a, T, MutexGuard<'a, Poison<T>>>,
-        > {
-            Poison::poison_target(self)
+    mod guard {
+        use super::*;
+
+        use std::sync::{Arc, Mutex};
+
+        struct DropValue(Arc<Mutex<usize>>);
+
+        impl ops::Drop for DropValue {
+            fn drop(&mut self) {
+                *self.0.lock().unwrap() += 1;
+            }
         }
-    }
 
-    impl<'a, T> ops::Deref for MutexGuard<'a, T> {
-        type Target = T;
+        #[test]
+        fn init_guard_ok() {
+            type ToInit = (usize, [mem::MaybeUninit<u8>; 16]);
 
-        fn deref(&self) -> &T {
-            &*self.0
+            let (_, init) = InitArgs {
+                to_init: (0, unsafe { mem::MaybeUninit::uninit().assume_init() }),
+                on_init: |(i, arr): &mut ToInit| {
+                    while *i < 16 {
+                        arr[*i] = mem::MaybeUninit::new(*i as u8);
+                        *i += 1;
+                    }
+                },
+                on_err_unwind: |(i, arr): &mut ToInit| {
+                    for i in 0..*i {
+                        unsafe {
+                            ptr::drop_in_place(arr[i].as_mut_ptr() as *mut u8);
+                        }
+                    }
+                },
+            }
+            .init();
+
+            let arr = unsafe { mem::transmute::<[mem::MaybeUninit<u8>; 16], [u8; 16]>(init) };
+
+            assert_eq!(
+                [0u8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+                arr
+            );
         }
-    }
 
-    impl<'a, T> ops::DerefMut for MutexGuard<'a, T> {
-        fn deref_mut(&mut self) -> &mut T {
-            &mut *self.0
+        #[test]
+        fn init_guard_try_ok() {
+            type ToInit = (usize, [mem::MaybeUninit<u8>; 16]);
+
+            let (_, init) = InitArgs {
+                to_init: (0, unsafe { mem::MaybeUninit::uninit().assume_init() }),
+                on_init: |(i, arr): &mut ToInit| {
+                    while *i < 16 {
+                        arr[*i] = mem::MaybeUninit::new(*i as u8);
+                        *i += 1;
+                    }
+
+                    Ok::<(), &'static str>(())
+                },
+                on_err_unwind: |(i, arr): &mut ToInit| {
+                    for i in 0..*i {
+                        unsafe {
+                            ptr::drop_in_place(arr[i].as_mut_ptr() as *mut u8);
+                        }
+                    }
+                },
+            }
+            .try_init()
+            .unwrap();
+
+            let arr = unsafe { mem::transmute::<[mem::MaybeUninit<u8>; 16], [u8; 16]>(init) };
+
+            assert_eq!(
+                [0u8, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+                arr
+            );
         }
-    }
 
-    #[test]
-    fn poisoning_mutex() {
-        let mutex = Mutex::new(Poison::new(42));
+        #[test]
+        fn init_guard_panic() {
+            type ToInit = (usize, [mem::MaybeUninit<DropValue>; 16]);
 
-        let mut guard = mutex.lock().poison().unwrap();
+            let mut init_count = 0;
+            let drop_count = Arc::new(Mutex::new(0));
 
-        *guard = 43;
-        drop(guard);
+            let p = Poison::catch_unwind(|| {
+                InitArgs {
+                    to_init: (0, unsafe { mem::MaybeUninit::uninit().assume_init() }),
+                    on_init: |(i, arr): &mut ToInit| {
+                        while *i < 16 {
+                            arr[*i] = mem::MaybeUninit::new(DropValue(drop_count.clone()));
+                            init_count += 1;
 
-        let guard = mutex.lock();
+                            *i += 1;
+                            if *i == 8 {
+                                panic!("lol");
+                            }
+                        }
+                    },
+                    on_err_unwind: |(i, arr): &mut ToInit| {
+                        for i in 0..*i {
+                            unsafe {
+                                ptr::drop_in_place(arr[i].as_mut_ptr() as *mut DropValue);
+                            }
+                        }
+                    },
+                }
+                .init();
+            });
 
-        assert!(!guard.is_poisoned());
+            assert!(p.is_poisoned());
 
-        let guard = guard.poison().unwrap();
-        assert_eq!(43, *guard);
-        drop(guard);
+            assert!(init_count > 0);
+            assert_eq!(init_count, *drop_count.lock().unwrap());
+        }
 
-        // Poison the guard without deadlocking the mutex
-        let mut v = mutex.lock().poison().unwrap();
-        v.unpoison_on_drop(false);
-        drop(v);
+        #[test]
+        fn init_guard_try_err() {
+            type ToInit = (usize, [mem::MaybeUninit<DropValue>; 16]);
 
-        let guard = mutex
-            .lock()
-            .poison()
-            .unwrap_or_else(|guard| guard.unpoison(|v| *v = 42));
+            let mut init_count = 0;
+            let drop_count = Arc::new(Mutex::new(0));
 
-        assert_eq!(42, *guard);
+            let p = Poison::catch_unwind(|| {
+                InitArgs {
+                    to_init: (0, unsafe { mem::MaybeUninit::uninit().assume_init() }),
+                    on_init: |(i, arr): &mut ToInit| {
+                        while *i < 16 {
+                            arr[*i] = mem::MaybeUninit::new(DropValue(drop_count.clone()));
+                            init_count += 1;
+
+                            *i += 1;
+                            if *i == 8 {
+                                return Err::<(), &'static str>("failed!");
+                            }
+                        }
+
+                        Ok::<(), &'static str>(())
+                    },
+                    on_err_unwind: |(i, arr): &mut ToInit| {
+                        for i in 0..*i {
+                            unsafe {
+                                ptr::drop_in_place(arr[i].as_mut_ptr() as *mut DropValue);
+                            }
+                        }
+                    },
+                }
+                .try_init()
+                .unwrap();
+            });
+
+            assert!(p.is_poisoned());
+
+            assert!(init_count > 0);
+            assert_eq!(init_count, *drop_count.lock().unwrap());
+        }
+
+        #[test]
+        fn init_guard_cheeky_mem_swap() {
+            unimplemented!("what can we break with `mem::swap` (internally in `InitArgs`)?")
+        }
     }
 }
